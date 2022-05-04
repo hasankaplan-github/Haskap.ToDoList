964ab4d7157fb1d02e22b0fd7a2288db757ca07b
Hasan Kaplan <haskap84@hotmail.com>
2022-05-04 13:03:31 +0300

    Unauthorized exception yönlendirmesi ayarlandı.

--- Haskap.ToDoList.Ui.MvcWebUi/Program.cs
+++ Haskap.ToDoList.Ui.MvcWebUi/Program.cs
@@ -1,4 +1,5 @@
 using Haskap.ToDoList.Ui.MvcWebUi.Services;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,10 +16,45 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    //app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseExceptionHandler(appBuilder =>
+{
+    appBuilder.Run(async context =>
+    {
+        //var isAjaxRequest = context.Request.Headers["X-Requested-With"] == "XMLHttpRequest";
+
+        //if (isAjaxRequest)
+        //{
+        //    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        //    context.Response.ContentType = Text.Plain;
+        //    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        //    var exception = exceptionHandlerPathFeature?.Error;
+        //    var exceptionMessage = exception?.Message;
+        //    await context.Response.WriteAsync(exceptionMessage);
+        //}
+        //else
+        //{
+        //    context.Response.Redirect("/Home/Error");
+        //}
+
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var exception = exceptionHandlerPathFeature?.Error;
+        if (exception is UnauthorizedAccessException)
+        {
+            context.Response.Redirect("/Account/Login");
+        }
+        else if (!app.Environment.IsDevelopment())
+        {
+            context.Response.Redirect("/Home/Error");
+        }
+    });
+});
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
--- Haskap.ToDoList.Ui.MvcWebUi/Services/ToDoListService.cs
+++ Haskap.ToDoList.Ui.MvcWebUi/Services/ToDoListService.cs
@@ -1,5 +1,6 @@
 ﻿using Haskap.ToDoList.Application.UseCaseServices.Dtos;
 using Microsoft.Net.Http.Headers;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
@@ -13,13 +14,13 @@ public class ToDoListService
     public ToDoListService(HttpClient httpClient, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
     {
         _httpClient = httpClient;
-        
+
         _httpClient.BaseAddress = new Uri(configuration["ToDoListApiBaseUrl"]);
         //_httpClient.DefaultRequestHeaders.Accept.Clear();
         //_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
         _httpClient.DefaultRequestHeaders.Add(HeaderNames.Authorization, $"Bearer {httpContextAccessor.HttpContext.Request.Cookies["jwt"]}");
-//        _httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "*/*");
+        //        _httpClient.DefaultRequestHeaders.Add(HeaderNames.Accept, "*/*");
         //_httpClient.DefaultRequestHeaders.Add(HeaderNames.ContentType, "application/json");
     }
 
@@ -43,6 +44,11 @@ public class ToDoListService
     {
         //var result = await _httpClient.GetFromJsonAsync<Envelope<IEnumerable<ToDoListOutputDto>>>("/ToDoList/List");
         var httpResponseMessage = await _httpClient.GetAsync("/ToDoList/List");
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
         var result = await httpResponseMessage.Content.ReadFromJsonAsync<Envelope<IEnumerable<ToDoListOutputDto>>>();
 
         return result;
@@ -50,22 +56,38 @@ public class ToDoListService
 
     public async Task AddToDoList(ToDoListInputDto toDoListInputDto)
     {
-        await _httpClient.PostAsync("/ToDoList", new StringContent(JsonSerializer.Serialize(toDoListInputDto), Encoding.UTF8, "application/json"));
+        var httpResponseMessage = await _httpClient.PostAsync("/ToDoList", new StringContent(JsonSerializer.Serialize(toDoListInputDto), Encoding.UTF8, "application/json"));
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     public async Task DeleteToDoList(Guid toDoListId)
     {
-        await _httpClient.DeleteAsync($"/ToDoList/{toDoListId}");
+        var httpResponseMessage = await _httpClient.DeleteAsync($"/ToDoList/{toDoListId}");
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     public async Task UpdateToDoList(Guid toDoListId, ToDoListInputDto toDoListInputDto)
     {
-        await _httpClient.PutAsync($"/ToDoList/{toDoListId}", new StringContent(JsonSerializer.Serialize(toDoListInputDto)));
+        var httpResponseMessage = await _httpClient.PutAsync($"/ToDoList/{toDoListId}", new StringContent(JsonSerializer.Serialize(toDoListInputDto)));
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     public async Task MarkAsCompleted(MarkAsCompleted_ToDoListInputDto toDoListInputDto)
     {
-        await _httpClient.PostAsync("/ToDoList/MarkAsCompleted", new StringContent(JsonSerializer.Serialize(toDoListInputDto), Encoding.UTF8, "application/json"));
+        var httpResponseMessage = await _httpClient.PostAsync("/ToDoList/MarkAsCompleted", new StringContent(JsonSerializer.Serialize(toDoListInputDto), Encoding.UTF8, "application/json"));
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     #endregion
@@ -74,22 +96,38 @@ public class ToDoListService
 
     public async Task AddToDoItem(ToDoItemInputDto toDoItemInputDto)
     {
-        await _httpClient.PostAsync("/ToDoItem", new StringContent(JsonSerializer.Serialize(toDoItemInputDto), Encoding.UTF8, "application/json"));
+        var httpResponseMessage = await _httpClient.PostAsync("/ToDoItem", new StringContent(JsonSerializer.Serialize(toDoItemInputDto), Encoding.UTF8, "application/json"));
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     public async Task DeleteToDoItem(Guid ownerToDoListId, Guid toDoItemId)
     {
-        await _httpClient.DeleteAsync($"/ToDoItem/{ownerToDoListId}/{toDoItemId}");
+        var httpResponseMessage = await _httpClient.DeleteAsync($"/ToDoItem/{ownerToDoListId}/{toDoItemId}");
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     public async Task UpdateToDoItem(Guid toDoItemId, ToDoItemInputDto toDoItemInputDto)
     {
-        await _httpClient.PutAsync($"/ToDoItem/{toDoItemId}", new StringContent(JsonSerializer.Serialize(toDoItemInputDto)));
+        var httpResponseMessage = await _httpClient.PutAsync($"/ToDoItem/{toDoItemId}", new StringContent(JsonSerializer.Serialize(toDoItemInputDto)));
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     public async Task<Envelope<IEnumerable<ToDoItemOutputDto>>> GetToDoItems(Guid ownerToDoListId)
     {
         var httpResponseMessage = await _httpClient.GetAsync($"/ToDoItem/List/{ownerToDoListId}");
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
         var result = await httpResponseMessage.Content.ReadFromJsonAsync<Envelope<IEnumerable<ToDoItemOutputDto>>>();
 
         return result;
@@ -97,12 +135,20 @@ public class ToDoListService
 
     public async Task MarkAsCompleted(MarkAsCompleted_ToDoItemInputDto toDoItemInputDto)
     {
-        await _httpClient.PostAsync("/ToDoItem/MarkAsCompleted", new StringContent(JsonSerializer.Serialize(toDoItemInputDto), Encoding.UTF8, "application/json"));
+        var httpResponseMessage = await _httpClient.PostAsync("/ToDoItem/MarkAsCompleted", new StringContent(JsonSerializer.Serialize(toDoItemInputDto), Encoding.UTF8, "application/json"));
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     public async Task MarkAsNotCompleted(MarkAsNotCompleted_ToDoItemInputDto toDoItemInputDto)
     {
-        await _httpClient.PostAsync("/ToDoItem/MarkAsNotCompleted", new StringContent(JsonSerializer.Serialize(toDoItemInputDto), Encoding.UTF8, "application/json"));
+        var httpResponseMessage = await _httpClient.PostAsync("/ToDoItem/MarkAsNotCompleted", new StringContent(JsonSerializer.Serialize(toDoItemInputDto), Encoding.UTF8, "application/json"));
+        if (httpResponseMessage.IsSuccessStatusCode == false && httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            throw new UnauthorizedAccessException();
+        }
     }
 
     #endregion
